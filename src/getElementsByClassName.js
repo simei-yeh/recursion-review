3e1975b287135431c0ee379f8c6d7b87a28e2ed6
Simei Yeh <simei.yeh@gmail.com>
2020-10-05 12:47:06 -0700

    initial recursion setup and comments

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,29 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  // create temp array to store elements with class name
+  let result = [];
+  let currElement = document.body;
+
+  console.log(currElement);
+
+  // iterate through the DOM structure
+  // check if current element has the class name
+  if (currElement.classList.contains(className)) {
+    result.push(currElement);
+  }
+
+  // check if it has an element child node
+  if (currElement.children) {
+    let children = currElement.children;
+
+    for (let i = 0; i < children.length; i++) {
+      currElement = children[i];
+      console.log(currElement);
+      getElementsByClassName(className);
+    }
+  }
+
+  //return an array of elements with specified class name
+  return result;
 };
